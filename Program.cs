f397600127c6235adc2ec103143ddcb33683436c
Artem Kopytko <artem.kop19@gmail.com>
2023-09-04 15:29:40 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _434._Number_of_Segments_in_a_String
+{
+    internal class Program
+    {
+        public static int CountSegments(string s)
+        {
+            return s.Split(' ').Where(w => w.Length != 0).Count();
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
